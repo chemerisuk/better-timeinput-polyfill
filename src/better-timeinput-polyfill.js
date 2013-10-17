69128e31ee0e2934aadb3554d20cecab9516105d
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-10-17 03:08:38 +0300

    use better-timeinput class in mobile browsers as well, drop focus class

--- src/better-timeinput-polyfill.js
+++ src/better-timeinput-polyfill.js
@@ -1,13 +1,12 @@
 (function(DOM, undefined) {
     "use strict";
 
-    if ("orientation" in window) return; // skip mobile and tablet browsers
-
     var HOURS_KEY = "hours-select",
         HOURS_TEMPLATE = DOM.template("select.better-timeinput-select>(option>{$$@6})*18+(option>{$$@0})*3"),
         MINUTES_KEY = "minutes-select",
         MINUTES_TEMPLATE = DOM.template("select.better-timeinput-select>option>{00}^option>{05}^(option>{$0}^option>{$5})*5"),
-        FOCUS_CLASS = "better-timeinput-focus";
+        NORMAL_CLASS = "better-timeinput",
+        extension = function() { this.addClass(NORMAL_CLASS) };
 
     function createSelect(input, key, template, defaultValue) {
         var el = DOM.create(template, {defaultValue: defaultValue});
@@ -16,37 +15,36 @@
 
         return el
             .on("change", input, "handleSelectChange")
-            .on("focus", input, "handleSelectFocus")
-            .on("blur", input, "handleSelectBlur");
+            .on("focus", input, "handleSelectFocus");
     }
 
-    DOM.extend("input[type=time]", {
-        constructor: function() {
-            var value = this.get().split(":");
+    if (!("orientation" in window)) {
+        // polyfill element for desktop browsers
+        extension = {
+            constructor: function() {
+                var value = this.get().split(":");
 
-            createSelect(this, HOURS_KEY, HOURS_TEMPLATE, value[0]);
-            createSelect(this, MINUTES_KEY, MINUTES_TEMPLATE, value[1]);
+                createSelect(this, HOURS_KEY, HOURS_TEMPLATE, value[0]);
+                createSelect(this, MINUTES_KEY, MINUTES_TEMPLATE, value[1]);
 
-            // remove legacy dateinput
-            // set tabindex=-1 because there are selects instead
-            this
-                .set({type: "text", autocomplete: "off", readonly: true, tabindex: "-1"})
-                .addClass("better-timeinput");
+                // remove legacy dateinput
+                // set tabindex=-1 because there are selects instead
+                this
+                    .set({type: "text", autocomplete: "off", readonly: true, tabindex: -1})
+                    .addClass(NORMAL_CLASS);
 
-            if (this.matches(":focus")) this.data(HOURS_KEY).fire("focus");
-        },
-        handleSelectChange: function(target) {
-            this
-                .set(this.data(HOURS_KEY).get() + ":" + this.data(MINUTES_KEY).get())
-                .handleSelectFocus(target);
-        },
-        handleSelectFocus: function(target) {
-            var start = target === this.data(HOURS_KEY) ? 0 : 3,
-                end = target === this.data(HOURS_KEY) ? 2 : 5;
+                if (this.matches(":focus")) this.data(HOURS_KEY).fire("focus");
+            },
+            handleSelectChange: function(target) {
+                this
+                    .set(this.data(HOURS_KEY).get() + ":" + this.data(MINUTES_KEY).get())
+                    .handleSelectFocus(target);
+            },
+            handleSelectFocus: function(target) {
+                var start = target === this.data(HOURS_KEY) ? 0 : 3,
+                    end = target === this.data(HOURS_KEY) ? 2 : 5;
 
-            this
-                .addClass(FOCUS_CLASS)
-                .legacy(function(node) {
+                this.legacy(function(node) {
                     if (node.setSelectionRange) {
                         node.setSelectionRange(start, end);
                     } else if (node.selectionStart !== undefined) {
@@ -54,9 +52,9 @@
                         node.selectionEnd = end;
                     }
                 });
-        },
-        handleSelectBlur: function() {
-            this.removeClass(FOCUS_CLASS);
-        }
-    });
+            }
+        };
+    }
+
+    DOM.extend("input[type=time]", extension);
 }(window.DOM));
