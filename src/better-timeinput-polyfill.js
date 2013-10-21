7517dbb730a93b8db894afa32488f7151e984e30
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-10-22 03:00:29 +0900

    drop extension var, add more code comments

--- src/better-timeinput-polyfill.js
+++ src/better-timeinput-polyfill.js
@@ -5,8 +5,7 @@
         HOURS_TEMPLATE = DOM.template("select.better-timeinput-select>(option>{$$@6})*18+(option>{$$@0})*3"),
         MINUTES_KEY = "minutes-select",
         MINUTES_TEMPLATE = DOM.template("select.better-timeinput-select>option>{00}^option>{05}^(option>{$0}^option>{$5})*5"),
-        NORMAL_CLASS = "better-timeinput",
-        extension = function() { this.addClass(NORMAL_CLASS) };
+        COMPONENT_CLASS = "better-timeinput";
 
     function createSelect(input, key, template, defaultValue) {
         var el = DOM.create(template, {defaultValue: defaultValue});
@@ -18,43 +17,40 @@
             .on("focus", input, "handleSelectFocus");
     }
 
-    if (!("orientation" in window)) {
+    DOM.extend("input[type=time]", "orientation" in window ? function() { this.addClass(COMPONENT_CLASS) } : {
         // polyfill element for desktop browsers
-        extension = {
-            constructor: function() {
-                var value = this.get().split(":");
+        constructor: function() {
+            var value = this.get().split(":");
 
-                createSelect(this, HOURS_KEY, HOURS_TEMPLATE, value[0]);
-                createSelect(this, MINUTES_KEY, MINUTES_TEMPLATE, value[1]);
+            createSelect(this, HOURS_KEY, HOURS_TEMPLATE, value[0]);
+            createSelect(this, MINUTES_KEY, MINUTES_TEMPLATE, value[1]);
 
-                // remove legacy dateinput
-                // set tabindex=-1 because there are selects instead
-                this
-                    .set({type: "text", autocomplete: "off", readonly: true, tabindex: -1})
-                    .addClass(NORMAL_CLASS);
+            // 1. remove legacy dateinput
+            // 2. set tabindex=-1 because there are hidden selects instead
+            this
+                .set({type: "text", autocomplete: "off", readonly: true, tabindex: -1})
+                .addClass(COMPONENT_CLASS);
 
-                if (this.matches(":focus")) this.data(HOURS_KEY).fire("focus");
-            },
-            handleSelectChange: function(target) {
-                this
-                    .set(this.data(HOURS_KEY).get() + ":" + this.data(MINUTES_KEY).get())
-                    .handleSelectFocus(target);
-            },
-            handleSelectFocus: function(target) {
-                var start = target === this.data(HOURS_KEY) ? 0 : 3,
-                    end = target === this.data(HOURS_KEY) ? 2 : 5;
+            if (this.matches(":focus")) this.data(HOURS_KEY).fire("focus");
+        },
+        handleSelectChange: function(target) {
+            this
+                .set(this.data(HOURS_KEY).get() + ":" + this.data(MINUTES_KEY).get())
+                .handleSelectFocus(target);
+        },
+        handleSelectFocus: function(target) {
+            var isHoursFocused = target === this.data(HOURS_KEY),
+                start = isHoursFocused ? 0 : 3,
+                end = isHoursFocused ? 2 : 5;
 
-                this.legacy(function(node) {
-                    if (node.setSelectionRange) {
-                        node.setSelectionRange(start, end);
-                    } else if (node.selectionStart !== undefined) {
-                        node.selectionStart = start;
-                        node.selectionEnd = end;
-                    }
-                });
-            }
-        };
-    }
-
-    DOM.extend("input[type=time]", extension);
+            this.legacy(function(node) {
+                if (node.setSelectionRange) {
+                    node.setSelectionRange(start, end);
+                } else if (node.selectionStart !== undefined) {
+                    node.selectionStart = start;
+                    node.selectionEnd = end;
+                }
+            });
+        }
+    });
 }(window.DOM));
