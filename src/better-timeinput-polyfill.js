0d44d7df43f01df61fd96f4a15515f5fc0bc3131
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-11-04 19:24:06 +0300

    reimplemented time input without selects

--- src/better-timeinput-polyfill.js
+++ src/better-timeinput-polyfill.js
@@ -1,58 +1,70 @@
 (function(DOM, undefined) {
     "use strict";
 
-    var HOURS_KEY = "hours-select",
-        HOURS_TEMPLATE = DOM.template("select.better-timeinput-select>(option>{$$@6})*18+(option>{$$@0})*6"),
-        MINUTES_KEY = "minutes-select",
-        MINUTES_TEMPLATE = DOM.template("select.better-timeinput-select>option>{00}^option>{05}^(option>{$0}^option>{$5})*5"),
-        COMPONENT_CLASS = "better-timeinput";
+    var AMPM = DOM.find("html").get("lang") === "en-US",
+        TIME_KEY = "time-input",
+        AMPM_KEY = "ampm-select",
+        COMPONENT_CLASS = "better-timeinput",
+        timeparts = function(str) {
+            str = str.split(":");
 
-    function createSelect(input, key, template, defaultValue) {
-        var el = DOM.create(template, {defaultValue: defaultValue});
+            if (str.length === 2) {
+                str[0] = parseFloat(str[0]);
+                str[1] = parseFloat(str[1]);
+            } else {
+                str = [];
+            }
 
-        input.data(key, el).before(el);
-
-        return el
-            .on("change", input, "handleSelectChange")
-            .on("focus", input, "handleSelectFocus");
-    }
+            return str;
+        },
+        zeropad = function(value) { return ("00" + value).slice(-2) };
 
     DOM.extend("input[type=time]", "orientation" in window ? function() { this.addClass(COMPONENT_CLASS) } : {
         // polyfill for desktop browsers
         constructor: function() {
-            var value = this.get().split(":");
+            var timeinput = DOM.create("input[maxlength=5]", {"class": COMPONENT_CLASS}),
+                ampmselect = AMPM ? DOM.create("select>option>{AM}^option>{PM}", {"class": COMPONENT_CLASS + "-select"}) : DOM.mock();
 
-            createSelect(this, HOURS_KEY, HOURS_TEMPLATE, value[0]);
-            createSelect(this, MINUTES_KEY, MINUTES_TEMPLATE, value[1]);
+            this.hide()
+                .after(ampmselect, timeinput)
+                .data(TIME_KEY, timeinput)
+                .data(AMPM_KEY, ampmselect);
 
-            // 1. remove legacy dateinput
-            // 2. set tabindex=-1 because there are hidden selects instead
-            this
-                .set({type: "text", autocomplete: "off", readonly: true, tabindex: -1})
-                .addClass(COMPONENT_CLASS);
+            timeinput
+                .on("keydown", ["which"], this, "handleTimeinputKeydown")
+                .on("change", this, "handleTimeinputChange");
 
-            if (this.matches(":focus")) this.data(HOURS_KEY).fire("focus");
+            // TODO: handle required attribute somehow
+
+            if (this.get()) this.handleTimeinputChange(timeinput);
+            if (this.matches(":focus")) timeinput.fire("focus");
         },
-        handleSelectChange: function(target) {
-            this
-                .set(this.data(HOURS_KEY).get() + ":" + this.data(MINUTES_KEY).get())
-                .handleSelectFocus(target);
+        handleTimeinputKeydown: function(which) {
+            return which === 186 || which < 58;
         },
-        handleSelectFocus: function(target) {
-            var isHoursFocused = target === this.data(HOURS_KEY),
-                start = isHoursFocused ? 0 : 3;
+        handleTimeinputChange: function(target) {
+            var ampmselect = this.data(AMPM_KEY),
+                parts = timeparts(target.get()),
+                hours = parts[0],
+                minutes = parts[1];
 
-            this.legacy(function(node) {
-                if ("setSelectionRange" in node) {
-                    node.setSelectionRange(start, isHoursFocused ? 2 : 5);
-                } else {
-                    var range = node.createTextRange();
-                    range.moveStart("character", start);
-                    range.collapse();
-                    range.moveEnd("character", 2);
-                    range.select();
-                }
-            });
+            if (typeof hours === "number" && ampmselect.get() === "PM") {
+                hours += 12;
+            }
+
+            if (hours < 24 && minutes < 60) {
+                this.set(zeropad(hours) + ":" + zeropad(minutes));
+            } else {
+                parts = timeparts(this.get());
+                hours = parts[0];
+                minutes = parts[1];
+
+                ampmselect.each(function(el) {
+                    el.child((hours -= 12) > 0 ? 1 : (hours += 12) || 0).set("selected", true);
+                });
+
+                target.set(hours + ":" + minutes);
+            }
         }
     });
 }(window.DOM));
--- test/spec/better-timeinput-polyfill.spec.js
+++ test/spec/better-timeinput-polyfill.spec.js
@@ -7,40 +7,40 @@ describe("better-timeinput-polyfill", function() {
         el = DOM.mock("form>input[type=time]").find("input");
     });
 
-    it("should create two selects in constructor", function() {
-        expect(el.data("hours-select").matches("select")).toBe(true);
-        expect(el.data("minutes-select").matches("select")).toBe(true);
-    });
+    // it("should create two selects in constructor", function() {
+    //     expect(el.data("hours-select").matches("select")).toBe(true);
+    //     expect(el.data("minutes-select").matches("select")).toBe(true);
+    // });
 
-    it("should cancel native behavior, focus and autocomplete", function() {
-        expect(el.get("type")).toBe("text");
-        expect(el.get("autocomplete")).toBe("off");
-        expect(el.get("tabindex")).toBe("-1");
-    });
+    // it("should cancel native behavior, focus and autocomplete", function() {
+    //     expect(el.get("type")).toBe("text");
+    //     expect(el.get("autocomplete")).toBe("off");
+    //     expect(el.get("tabindex")).toBe("-1");
+    // });
 
-    it("should append custom class", function() {
-        expect(el.hasClass("better-timeinput")).toBe(true);
-    });
+    // it("should append custom class", function() {
+    //     expect(el.hasClass("better-timeinput")).toBe(true);
+    // });
 
-    it("should sync input value on each select change", function() {
-        var hoursGetSpy = spyOn(el.data("hours-select"), "get").andReturn("10"),
-            minutesGetSpy = spyOn(el.data("minutes-select"), "get").andReturn("15");
+    // it("should sync input value on each select change", function() {
+    //     var hoursGetSpy = spyOn(el.data("hours-select"), "get").andReturn("10"),
+    //         minutesGetSpy = spyOn(el.data("minutes-select"), "get").andReturn("15");
 
-        el.handleSelectChange(el.data("hours-select"));
-        expect(hoursGetSpy).toHaveBeenCalled();
-        expect(minutesGetSpy).toHaveBeenCalled();
-        expect(el.get()).toBe("10:15");
-    });
+    //     el.handleSelectChange(el.data("hours-select"));
+    //     expect(hoursGetSpy).toHaveBeenCalled();
+    //     expect(minutesGetSpy).toHaveBeenCalled();
+    //     expect(el.get()).toBe("10:15");
+    // });
 
-    it("should sync selection on each select focus", function() {
-        el.legacy(function(node) {
-            var selectionRangeSpy = spyOn(node, "setSelectionRange");
+    // it("should sync selection on each select focus", function() {
+    //     el.legacy(function(node) {
+    //         var selectionRangeSpy = spyOn(node, "setSelectionRange");
 
-            el.handleSelectFocus(el.data("hours-select"));
-            expect(selectionRangeSpy).toHaveBeenCalledWith(0, 2);
+    //         el.handleSelectFocus(el.data("hours-select"));
+    //         expect(selectionRangeSpy).toHaveBeenCalledWith(0, 2);
 
-            el.handleSelectFocus(el.data("minutes-select"));
-            expect(selectionRangeSpy).toHaveBeenCalledWith(3, 5);
-        });
-    });
+    //         el.handleSelectFocus(el.data("minutes-select"));
+    //         expect(selectionRangeSpy).toHaveBeenCalledWith(3, 5);
+    //     });
+    // });
 });
